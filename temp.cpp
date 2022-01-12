10a40f74af0855de493f745990ad58fd219edddb
Monkey@monkey-hyx.tech <monkey-hyx@qq.com>
2022-01-12 10:29:32 +0800

    update.

---
+++ temp.cpp
@@ -0,0 +1,32 @@
+
+
+#include <bits/stdc++.h>
+#include <unistd.h>
+using namespace std;
+typedef unsigned long long unll;
+typedef long long ll;
+template <typename T = int>
+inline T read(void);
+int main(){
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
