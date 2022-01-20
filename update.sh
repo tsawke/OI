64c661f9d78b83507b104247df7a5cfdbf5630ef
Monkey@monkey-hyx.tech <monkey-hyx@qq.com>
2022-01-20 08:44:37 +0800

    Generally Update - 2022-01-20_08:44:37 - By 123.56.4.12

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-time = $(date "+%Y-%m-%d_%H:%M:%S")
+time=$(date "+%Y-%m-%d_%H:%M:%S")
 
 git add .
-git commit -m ````
+git commit -m ``"Generally Update - $time - By 123.56.4.12"``
+git push
