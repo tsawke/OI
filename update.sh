941e386e1f6a6a56c78c5cb9f59c4048a5ea83b8
monkey@DELL <monkey-hyx@qq.com>
2022-02-10 14:00:20 +0800

    Generally Update - 2022-02-10_14:00:20 - By Monkey@123.56.4.12

--- update.sh
+++ update.sh
@@ -3,5 +3,5 @@
 time=$(date "+%Y-%m-%d_%H:%M:%S")
 
 git add .
-git commit -m ``"Generally Update - $time - By 123.56.4.12"``
+git commit -m ``"Generally Update - $time - By Monkey@123.56.4.12"``
 git push
