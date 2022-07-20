c761cb83a3ca6104c9f5667f90773195977c9e01
monkey@DELL <monkey-hyx@qq.com>
2022-07-20 15:51:24 +0800

    Generally Update - 2022-07-20_15:51:24 - By Monkey@123.56.4.12

---
+++ Luogu/P2XXX-P5XXX/P2602-Reference.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long a,b;
+long long ten[20],f[20];
+long long cnta[20],cntb[20];
+void solve(long long x,long long *cnt)
+{
+    long long num[20]={0};
+    int len=0;
+    while(x)
+    {
+        num[++len]=x%10;
+        x=x/10;
+    } 
+    for(int i=len;i>=1;i--)
+    {
+        for(int j=0;j<=9;j++)
+        cnt[j]+=f[i-1]*num[i];
+        for(int j=0;j<num[i];j++)
+        cnt[j]+=ten[i-1];
+        long long num2=0;
+        for(int j=i-1;j>=1;j--)
+        {
+            num2=num2*10+num[j];
+        }
+        cnt[num[i]]+=num2+1;
+        cnt[0]-=ten[i-1];
+    } 
+}
+int main()
+{
+    freopen("./P2602-RefOUTPUT.txt", "w", stdout);
+    // scanf("%lld %lld",&a,&b);
+    ten[0]=1;
+    for(int i=1;i<=15;i++)
+    {
+        f[i]=f[i-1]*10+ten[i-1];
+        ten[i]=10*ten[i-1];
+    }
+    // solve(a-1,cnta);
+    // solve(b,cntb);
+    // for(int i=0;i<=9;i++)
+    // printf("%lld ",cntb[i]-cnta[i]);
+
+    for(int i = 1; i <= 1000; ++i){
+        long long ans[20];
+        memset(ans, 0ll, sizeof(ans));
+        solve(i, ans);
+        printf("No.%d  [0]=>%lld%s", i, ans[0] + 1, (i % 5 == 0 || i == 1000) ? "\n" : "    ");
+        // ans.clear();
+    }
+}
--- Luogu/P2XXX-P5XXX/P2602.cpp
+++ Luogu/P2XXX-P5XXX/P2602.cpp
@@ -24,33 +24,65 @@ typedef long long ll;
 template <typename T = int>
 inline T read(void);
 
-ll a, b;
-ll f[15][15][15];//i位数 j开头 k有多少个
+unll a, b;
+unll f[15][15][15];//i位数 j开头 k有多少个
+unll f0[15][15][15];
+// ll lastZero[15][15];
+// vector < tuple<int, int, int, int>/*i, j, k, value*/ > oper;
 void Init(void);
-vector<int> DP(ll);
+vector<int> DP(unll);
 unll CreateNum(int, int, vector<int>);
+unll qPow(unll, unll, unll = ULONG_LONG_MAX);
+
 signed main(){
+    freopen("./P2602-OUTPUT.txt", "w", stdout);
     Init();
+    
+    for(int i = 1; i <= 4; ++i){
+        printf("I = %llu:\n", i);
+        for(int j = 0; j <= 9; ++j){
+            printf("            ");
+            for(int k = 0; k <= 9; ++k)printf("%llu ", f[i][j][k]);
+            printf("\n");
+        }
+    }
 
-    // for(int i = 1; i <= 3; ++i){
-    //     printf("I = %llu:\n", i);
-    //     for(int j = 0; j <= 9; ++j){
-    //         printf("            ");
-    //         for(int k = 0; k <= 9; ++k)printf("%llu ", f[i][j][k]);
-    //         printf("\n");
-    //     }
-    // }
+    for(int i = 1; i <= 4; ++i){
+        printf("I = %llu:\n", i);
+        for(int j = 0; j <= 9; ++j){
+            printf("            ");
+            for(int k = 0; k <= 9; ++k)printf("%llu ", f0[i][j][k]);
+            printf("\n");
+        }
+    }
 
-	a = read<ll>(), b = read<ll>();
-    vector <int> ansa ( DP(a - 1) );
-    vector <int> ansb ( DP(b) );
-    for(int i = 0; i <= 9; ++i)ansb.at(i) -= ansa.at(i);
-    // for(auto i : ansa)printf("%lld ", i);
+	// a = read<unll>(), b = read<unll>();
+    // vector <int> ansa ( DP(a - 1) );
+    // vector <int> ansb ( DP(b) );
+    // for(int i = 0; i <= 9; ++i)ansb.at(i) -= ansa.at(i);
+    // // for(auto i : ansa)printf("%lld ", i);
+    // // printf("\n");
+    // for(auto i : ansb)printf("%llu ", i);
     // printf("\n");
-    for(auto i : ansb)printf("%llu ", i);
-    printf("\n");
 
+    for(int i = 1; i <= 1000; ++i){
+        vector <int> ans (DP(i));
+        printf("No.%llu  [0]=>%llu%s", i, ans.at(0), (i % 5 == 0 || i == 1000) ? "\n" : "    ");
+        ans.clear();
+    }
     return 0;
+}
+unll qPow(unll a, unll b, unll MOD){
+    unll mul(a);
+    unll ret(1ull);
+    while(b){
+        if(b & 1ull){
+            ret = (ret * mul) % MOD;
+        }
+        mul = (mul * mul) % MOD;
+        b >>= 1;
+    }
+    return ret;
 }
 unll CreateNum(int s, int e, vector<int>nums){
     unll ret(0ull);
@@ -58,45 +90,90 @@ unll CreateNum(int s, int e, vector<int>nums){
     for(int i = e; i >= s; --i)ret += nums.at(i - 1) * mul, mul *= 10ull;
     return ret;
 }
-vector<int> DP(ll n){
+vector<int> DP(unll n){
     vector < int > nums;
     if(!n){
         vector <int> ans0(10, 0);
-        ans0.at(0) = 1;
+        ans0.at(0) = 1ull;
         return ans0;
     }
     while(n)nums.push_back(n % 10ull), n /= 10ull;
     reverse(nums.begin(), nums.end());
-    vector < int > anss(10, 0);
+    vector < int > anss(10, 0ull);
     int len(nums.size());
-    int s = 0;
-    int lastN(10);//return error
-    for(vector<int>::iterator itea = nums.begin(); itea != nums.end(); ++itea){
-        for(int i = 0; i < *itea; ++i){
+    // int s = 0;
+    // int lastN(10);//return error
+    // for(vector<int>::iterator itea = nums.begin(); itea != nums.end(); ++itea){
+    //     for(int i = itea == nums.begin() ? 1 : 0; i < *itea; ++i){
+    //         for(int r = 0; r <= 9; ++r){
+    //             anss[r] += f[len][i][r];
+    //         }
+    //     }
+    //     ++s;
+    //     --len;
+    //     if(itea != nums.begin())anss[lastN] += CreateNum(s, nums.size(), nums);
+    //     lastN = *itea;
+    // }
+    // for(int i = 2; i <= nums.size(); ++i){
+    //     for(int r = 0; r <= 9; ++r){
+    //         anss[r] += f0[nums.size() - i + 1][0][r];
+    //     }
+    // }
+    
+
+    for(int i = 1; i <= len - 1; ++i)
+        for(int j = i == 1 ? 0 : 1; j <= 9; ++j)
+            for(int r = 0; r <= 9; ++r)
+                anss.at(r) += f[i][j][r];
+    int LastNums[10];
+    memset(LastNums, 0ull, sizeof(LastNums));
+    int s(0ull);
+    for(vector< int >::iterator itea = nums.begin(); itea != nums.end(); ++itea){
+        for(int j = itea == nums.begin() ? 1 : 0; j < *itea; ++j){
             for(int r = 0; r <= 9; ++r){
-                anss[r] += f[len][i][r];
+                anss.at(r) += f[len][j][r];
             }
         }
         ++s;
-        --len;
-        if(itea != nums.begin())anss[lastN] += CreateNum(s, nums.size(), nums);
-        lastN = *itea;
+        if(itea != nums.begin()){
+            unll subNum = CreateNum(s, nums.size(), nums);
+            for(int r = 0; r <= 9; ++r){
+                anss[r] += LastNums[r] * subNum;
+            }
+        }
+        LastNums[*itea]++;
+
     }
     for(auto i : nums)++anss[i];
+    
     return anss;
 }
 void Init(void){
     memset(f, 0, sizeof(f));
     for(int i = 0; i <= 9; ++i)f[1][i][i] = 1;
     for(int i = 2; i <= 12; ++i){
+        // memset(lastZero, 0, sizeof(lastZero));
         for(int j = 0; j <= 9; ++j){
-            if(j)f[i][j][j] += (int)pow((float)10, (float)i - 1);
+            /*if(j)*/ f[i][j][j] += qPow(10ull, (unll)i - 1);
+            // if(!j) oper.push_back(make_tuple(i, j, j, (int)pow((float)10, (float)i - 1)));
             for(int r = 0; r <= 9; ++r){
+                // if(!r && j)f[i][j][0] += (int)pow((float)10, (float)i - 2);
                 for(int k = 0; k <= 9; ++k){
                     f[i][j][k] += f[i - 1][r][k];
+                    // if(!k)f[i][j][k] += lastZero[k][k];
                 }
             }
         }
+    }
+    // for(auto I : oper){
+    //     int i, j, k, value;
+    //     tie(i, j, k, value) = I;
+    //     f[i][j][k] -= value;
+    // }
+    // copy(begin(f), end(f), begin(f0));
+    memcpy(f0, f, sizeof(f));
+    for(int i = 2; i <= 12; ++i){
+        for(int j = 1; j <= i - 1; ++j)f0[i][0][0] -= qPow(10ull, (unll)i - j);
     }
 }
 template <typename T = int>
