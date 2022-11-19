e5f0c1e821e8cf4199b592a9a6ccf70ae21947a9
Monkey@jdoi-virtual-machine <monkey-hyx@qq.com>
2022-11-19 17:26:00 +0800

    Generally Update - 2022-11-19_17:26:00 - By Monkey@jdoi-virtual-machine

--- AtCoder/ABC/ABC248F.cpp
+++ AtCoder/ABC/ABC248F.cpp
@@ -27,7 +27,7 @@ int dp[3100][3100][2];
 
 int main(){
     N = read(), MOD = read();
-    dp[1][0][1] = 1;
+    dp[1][0][1] = dp[1][1][0] = 1;
     for(int i = 1; i <= N - 1; ++i)
         for(int j = 0; j <= N - 1; ++j)
             dp[i + 1][j + 1][0] = ((ll)dp[i + 1][j + 1][0] + dp[i][j][0]) % MOD,
