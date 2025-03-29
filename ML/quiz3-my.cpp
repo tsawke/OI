0262d210d9b64ca997f4a77202de0e39e1037b23
tsawke@desktop <12413104@mail.sustech.edu.cn>
2025-03-29 20:48:14 +0800

    Generally update from dell@tsawke at 2025.03.29 20:48:14

--- ML/quiz3-my.cpp
+++ ML/quiz3-my.cpp
@@ -1,14 +1,17 @@
 #include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix.hpp>
 #include <bits/stdc++.h>
 
 namespace qi = boost::spirit::qi;
 namespace ascii = boost::spirit::ascii;
 
-template < typename Iterator >
-class wffParser : qi::grammar < Iterator, ascii::space_type > {
-    qi::rule < Iterator, ascii::space_type > formula;
-    qi::rule < Iterator, ascii::space_type > variable;
-    qi::rule < Iterator, ascii::space_type > operator;
+// using namespace std;
+using std::string;
+
+struct wffParser : qi::grammar <string::const_iterator, ascii::space_type> {
+    qi::rule <string::const_iterator, ascii::space_type> formula;
+    qi::rule <string::const_iterator, ascii::space_type> variable;
+    qi::rule <string::const_iterator, ascii::space_type> binaryOperator;
 
     wffParser() : wffParser::base_type(formula) {
         using qi::lit;
@@ -17,5 +20,24 @@ class wffParser : qi::grammar < Iterator, ascii::space_type > {
         using ascii::alpha;
         
         variable = lexeme[alpha];
+
+        
+
+        formula = 
+            variable
+            | ('(' > lit("¬") > formula > ')')
+            | ('(' > formula > binaryOperator > formula > ')');
+87      binaryOperator = lit("∧") | lit("∨") | lit("→") | lit("↔");
+
     }
 };
+
+int main() {
+    wffParser parser;
+    string S;
+    std::getline(std::cin, S);
+    bool res = qi::phrase_parse(S.begin(), S.end(), parser, ascii::space);
+
+    printf("%s\n", res ? "It's a wff." : "It's not a wff.");
+
+}
