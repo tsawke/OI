eefb547b5995c11ba726b283b95e2c8692a9040f
tsawke@dorm <tsawke@outlook.com>
2025-11-18 02:47:54 +0800

    update

--- DSAA/Lab10/A.cpp
+++ DSAA/Lab10/A.cpp
@@ -11,6 +11,7 @@ int rndd(int l, int r){return rnd() % (r - l + 1) + l;}
 
 using ll = long long;
 using unll = unsigned long long;
+using uint = uint;
 using ld = long double;
 
 template < typename T = int >
@@ -20,270 +21,163 @@ inline T read(void){
     char c = getchar();
     while(c != '-' && !isdigit(c))c = getchar();
     if(c == '-')flag = -1, c = getchar();
-    while(isdigit(c))ret = ret * 10 + (c - '0'), c = getchar();
-    return ret * flag;
+    while(isdigit(c)){
+        ret *= 10;
+        ret += int(c - '0');
+        c = getchar();
+    }
+    ret *= flag;
+    return ret;
 }
 
-struct Node{
+class Node{
+public:
     Node *ls, *rs;
-    int val;
-    int siz;
-    int cnt;
-    int h;
+    int val, siz, cnt, h;
 };
 
-Node* root = nullptr;
+Node *root;
 
-#define siz(p)    ((p) ? (p)->siz : 0)
-#define height(p) ((p) ? (p)->h   : 0)
+#define siz(p) ((p) ? (p)->siz : 0)
+#define height(p) ((p) ? (p)->h : 0)
 
 class Tree{
+private:
 public:
-    // 维护子树大小和高度
-    void Pushup(Node* p){
-        if(!p) return;
+    void Pushup(Node *p){
+        if(!p)return;
         p->siz = siz(p->ls) + siz(p->rs) + p->cnt;
-        p->h   = max(height(p->ls), height(p->rs)) + 1;
+        p->h = max(height(p->ls), height(p->rs)) + 1;
     }
-
-    // 平衡因子 = 左高 - 右高
-    int GetBalance(Node* p){
-        if(!p) return 0;
+    int GetBalance(Node *p){
+        if(!p)return 0;
         return height(p->ls) - height(p->rs);
     }
-
-    // 右旋
-    Node* RotateRight(Node* y){
-        Node* x  = y->ls;
-        Node* T2 = x->rs;
-        x->rs = y;
-        y->ls = T2;
-        Pushup(y);
-        Pushup(x);
+    Node* RotateRight(Node *y){
+        Node *x = y->ls;
+        Node *T2 = x->rs;
+        x->rs = y, y->ls = T2;
+        Pushup(y), Pushup(x);
         return x;
     }
-
-    // 左旋
-    Node* RotateLeft(Node* x){
-        Node* y  = x->rs;
-        Node* T2 = y->ls;
-        y->ls = x;
-        x->rs = T2;
-        Pushup(x);
-        Pushup(y);
+    Node* RotateLeft(Node *x){
+        Node *y = x->rs;
+        Node *T2 = y->ls;
+        y->ls = x, x->rs = T2;
+        Pushup(x), Pushup(y);
         return y;
     }
-
-    Node* QueryMx(Node* p = root){
-        if(!p) return p;
-        while(p->rs) p = p->rs;
+    Node* QueryMx(Node *p = root){
+        if(!p)return p;
+        while(p->rs)p = p->rs;
         return p;
     }
-
-    Node* QueryMn(Node* p = root){
-        if(!p) return p;
-        while(p->ls) p = p->ls;
+    Node* QueryMn(Node *p = root){
+        if(!p)return p;
+        while(p->ls)p = p->ls;
         return p;
     }
-
-    // AVL 插入，返回当前子树的新根
-    Node* Insert(int val, Node* p = root){
-        if(!p) return new Node(val);
-
-        if(val < p->val){
-            p->ls = Insert(val, p->ls);
-        }else if(val > p->val){
-            p->rs = Insert(val, p->rs);
-        }else{
-            // 多重集：相同键只增加 cnt
+    Node* Insert(int val, Node *p = root){
+        if(!p)return new Node{nullptr, nullptr, val, 1, 1, 1};
+        if(val < p->val)p->ls = Insert(val, p->ls);
+        else if(val > p->val)p->rs = Insert(val, p->rs);
+        else{
             ++p->cnt;
             Pushup(p);
             return p;
         }
-
-        // 回溯更新高度和 size
         Pushup(p);
-
         int bf = GetBalance(p);
-
-        // 四种失衡情况
-
-        // LL
-        if(bf > 1 && val < p->ls->val)
-            return RotateRight(p);
-
-        // RR
-        if(bf < -1 && val > p->rs->val)
-            return RotateLeft(p);
-
-        // LR
-        if(bf > 1 && val > p->ls->val){
-            p->ls = RotateLeft(p->ls);
-            return RotateRight(p);
-        }
-
-        // RL
-        if(bf < -1 && val < p->rs->val){
-            p->rs = RotateRight(p->rs);
-            return RotateLeft(p);
-        }
-
+        if(bf > 1 && val < p->ls->val)return RotateRight(p);
+        if(bf < -1 && val > p->rs->val)return RotateLeft(p);
+        if(bf > 1 && val > p->ls->val)p->ls = RotateLeft(p->ls), p = RotateRight(p);
+        else if(bf < -1 && val < p->rs->val)p->rs = RotateRight(p->rs), p = RotateLeft(p);
         return p;
     }
-
-    // AVL 删除，返回当前子树的新根
-    Node* Delete(int val, Node* p = root){
-        if(!p) return p;
-
-        if(val < p->val){
-            p->ls = Delete(val, p->ls);
-        }else if(val > p->val){
-            p->rs = Delete(val, p->rs);
-        }else{
-            // 找到要删的结点
-            if(p->cnt > 1){
-                // 多重集：有多个，先减一
-                --p->cnt;
-            }else if(!p->ls || !p->rs){
-                // 度为 0 或 1
-                Node* tmp = p->ls ? p->ls : p->rs;
+    Node* Delete(int val, Node *p = root){
+        if(!p)return p;
+        if(val < p->val)p->ls = Delete(val, p->ls);
+        else if(val > p->val)p->rs = Delete(val, p->rs);
+        else{
+            if(p->cnt > 1)--p->cnt;
+            else if(!p->ls || !p->rs){
+                Node *tmp = p->ls ? p->ls : p->rs;
                 if(!tmp){
                     delete p;
                     return nullptr;
-                }else{
-                    Node* old = p;
-                    p = tmp;
-                    delete old;
-                }
+                }else delete exchange(p, tmp);
             }else{
-                // 度为 2：用右子树最小结点(后继)顶替
-                Node* succ = QueryMn(p->rs);
-                // 把后继的键值和计数整体搬上来
-                p->val = succ->val;
-                p->cnt = succ->cnt;
-                // 让真正的后继结点只剩 1 个, 方便彻底删掉它
+                Node *succ = QueryMn(p->rs);
+                p->val = succ->val, p->cnt = succ->cnt;
                 succ->cnt = 1;
-                // 在右子树里删掉这个后继结点
                 p->rs = Delete(succ->val, p->rs);
             }
         }
-
-        // 子树被删空
-        if(!p) return p;
-
-        // 回溯维护高度和 size
+        if(!p)return p;
         Pushup(p);
-
         int bf = GetBalance(p);
-
-        // 左高：可能 LL 或 LR
         if(bf > 1){
-            if(GetBalance(p->ls) >= 0){
-                // LL
-                return RotateRight(p);
-            }else{
-                // LR
-                p->ls = RotateLeft(p->ls);
-                return RotateRight(p);
-            }
+            if(GetBalance(p->ls) >= 0)return RotateRight(p);
+            p->ls = RotateLeft(p->ls);
+            return RotateRight(p);
         }
-
-        // 右高：可能 RR 或 RL
         if(bf < -1){
-            if(GetBalance(p->rs) <= 0){
-                // RR
-                return RotateLeft(p);
-            }else{
-                // RL
-                p->rs = RotateRight(p->rs);
-                return RotateLeft(p);
-            }
+            if(GetBalance(p->rs) <= 0)return RotateLeft(p);
+            p->rs = RotateRight(p->rs);
+            return RotateLeft(p);
         }
-
         return p;
     }
-
-    // 查询有多少数 < val
-    int QueryRnk(int val, Node* p = root){
-        if(!p) return 0;
-        if(val == p->val) return siz(p->ls);
-        if(val < p->val)  return QueryRnk(val, p->ls);
+    int QueryRnk(int val, Node *p = root){
+        if(!p)return 0;
+        if(val == p->val)return siz(p->ls);
+        if(val < p->val)return QueryRnk(val, p->ls);
         return siz(p->ls) + p->cnt + QueryRnk(val, p->rs);
     }
-
-    // 按排名查询：第 rnk 小的结点
-    Node* QueryByRnk(int rnk, Node* p = root){
-        if(!p) return p;
-        if(rnk <= siz(p->ls)) return QueryByRnk(rnk, p->ls);
-        if(rnk <= siz(p->ls) + p->cnt) return p;
+    Node* QueryByRnk(int rnk, Node *p = root){
+        if(!p)return p;
+        if(rnk <= siz(p->ls))return QueryByRnk(rnk, p->ls);
+        if(rnk <= siz(p->ls) + p->cnt)return p;
         return QueryByRnk(rnk - siz(p->ls) - p->cnt, p->rs);
     }
-
-    // 后继：严格大于 val 的最小值
-    Node* QuerySuc(int val, Node* p = root){
-        if(!p) return p;
-        if(val >= p->val) return QuerySuc(val, p->rs);
-        Node* res = QuerySuc(val, p->ls);
+    Node* QuerySuc(int val, Node *p = root){
+        if(!p)return p;
+        if(val >= p->val)return QuerySuc(val, p->rs);
+        Node *res = QuerySuc(val, p->ls);
         return res ? res : p;
     }
-
-    // 前驱：严格小于 val 的最大值
-    Node* QueryPre(int val, Node* p = root){
-        if(!p) return p;
-        if(val <= p->val) return QueryPre(val, p->ls);
-        Node* res = QueryPre(val, p->rs);
+    Node* QueryPre(int val, Node *p = root){
+        if(!p)return p;
+        if(val <= p->val)return QueryPre(val, p->ls);
+        Node *res = QueryPre(val, p->rs);
         return res ? res : p;
+    }
+    void DfsPre(Node *p, uint &idx, uint &ans){
+        if(!p)return;
+        ans += idx++ ^ (uint)abs(p->val);
+        DfsPre(p->ls, idx, ans);
+        DfsPre(p->rs, idx, ans);
     }
 }tr;
 
-// 先序遍历计算校验和：
-// ans += (idx ^ |val|), idx 从 1 开始
-void PreOrderChecksum(Node* p, unsigned int &idx, unsigned int &ans){
-    if(!p) return;
-    ans += (idx ^ (unsigned int)std::abs(p->val));
-    ++idx;
-    PreOrderChecksum(p->ls, idx, ans);
-    PreOrderChecksum(p->rs, idx, ans);
-}
-
 int main(){
-    int T = read<int>();
+    int T = read();
     while(T--){
-        int opt = read<int>(), val = read<int>();
+        int opt = read(), val = read();
         switch(opt){
-            case 1:
-                root = tr.Insert(val, root);
-                break;
-            case 2:
-                root = tr.Delete(val, root);
-                break;
-            case 3:
-                printf("%d\n", tr.QueryRnk(val, root) + 1);
-                break;
-            case 4:{
-                Node* p = tr.QueryByRnk(val, root);
-                printf("%d\n", p->val);
-                break;
-            }
-            case 5:{
-                Node* p = tr.QueryPre(val, root);
-                printf("%d\n", p->val);
-                break;
-            }
-            case 6:{
-                Node* p = tr.QuerySuc(val, root);
-                printf("%d\n", p->val);
-                break;
-            }
+            case 1: root = tr.Insert(val); break;
+            case 2: root = tr.Delete(val); break;
+            case 3: printf("%d\n", tr.QueryRnk(val) + 1); break;
+            case 4: printf("%d\n", tr.QueryByRnk(val)->val); break;
+            case 5: printf("%d\n", tr.QueryPre(val)->val); break;
+            case 6: printf("%d\n", tr.QuerySuc(val)->val); break;
         }
     }
-
-    // 如果题目要求输出 AVL 校验和，则保留下面这段
-    // 如果不需要校验和，可以直接删掉
-    unsigned int idx = 1;
-    unsigned int ans = 0;
-    PreOrderChecksum(root, idx, ans);
+    uint idx = 1, ans = 0;
+    tr.DfsPre(root, idx, ans);
     printf("%u\n", ans);
 
+    // fprintf(stderr, "Time: %.6lf\n", (double)clock() / CLOCKS_PER_SEC);
     return 0;
 }
+    `   
